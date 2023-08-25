05b5059eaa41eaceb0dd9e3787b7b316a2073a79
Agusmac <agusmac40@gmail.com>
2023-08-25 01:39:04 -0300

    fixed img src

--- components/Loader.js
+++ components/Loader.js
@@ -14,7 +14,7 @@ export default function Loader() {
          bg-[#f4f4f4] inset-0 text-[#252422] grid place-content-center noise delay-500 ${test ? 'translate-y-[-100%] ' : 'translate-y-0'}`}>
 
             <div className={`relative overflow-hidden duration-500 ease-[cubic-bezier(0.5,0,0,1)] delay-[250ms] ${test ? 'translate-y-[-101%]' : 'translate-y-0'}`} >
-                <Image priority className={`w-[35vw] h-auto duration-500 ease-[cubic-bezier(0.5,0,0,1)] delay-[250ms] ${test ? 'translate-y-[102%]' : ' translate-y-0'}`} src='/Assets/BDLoaderLogo.svg' alt='BD-logo' quality={100} width={672.16} height={315.22} />
+                <Image priority className={`w-[35vw] h-auto duration-500 ease-[cubic-bezier(0.5,0,0,1)] delay-[250ms] ${test ? 'translate-y-[102%]' : ' translate-y-0'}`} src='/Assets/BDLoaderlogo.svg' alt='BD-logo' quality={100} width={672.16} height={315.22} />
             </div>
         </div>
     )
